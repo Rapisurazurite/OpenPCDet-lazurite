0172faab2067edfa4e1dcf6795945c97bd50a1f7
Lazurite <lazurite@tom.com>
2022-04-17 15:23:29 +0800

    完整版配置

--- tools/train_with_test.py
+++ tools/train_with_test.py
@@ -44,7 +44,7 @@ def parse_config():
 
     parser.add_argument('--max_waiting_mins', type=int, default=0, help='max waiting minutes')
     parser.add_argument('--start_epoch', type=int, default=0, help='')
-    parser.add_argument('--num_epochs_to_eval', type=int, default=0, help='number of checkpoints to be evaluated')
+    parser.add_argument('--num_epochs_to_eval', type=int, default=30, help='number of checkpoints to be evaluated')
     parser.add_argument('--save_to_file', action='store_true', default=False, help='')
 
     args = parser.parse_args()
